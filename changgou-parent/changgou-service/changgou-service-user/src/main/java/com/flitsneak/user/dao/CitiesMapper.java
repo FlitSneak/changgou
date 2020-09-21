97f70e5d53be1d45523120616384f968ad5d81cf
FlitSneak <870295595@qq.com>
2020-09-22 00:19:44 +0800

    加入gateway网关配置限流，初步实现jwt鉴权。

---
+++ changgou-parent/changgou-service/changgou-service-user/src/main/java/com/flitsneak/user/dao/CitiesMapper.java
@@ -0,0 +1,11 @@
+package com.flitsneak.user.dao;
+import com.flitsneak.user.pojo.Cities;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:admin
+ * @Description:Cities的Dao
+ * @Date 2019/6/14 0:12
+ *****/
+public interface CitiesMapper extends Mapper<Cities> {
+}
---
+++ changgou-parent/changgou-service/changgou-service-user/src/main/java/com/flitsneak/user/dao/UndoLogMapper.java
@@ -0,0 +1,11 @@
+package com.flitsneak.user.dao;
+import com.flitsneak.user.pojo.UndoLog;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:admin
+ * @Description:UndoLog的Dao
+ * @Date 2019/6/14 0:12
+ *****/
+public interface UndoLogMapper extends Mapper<UndoLog> {
+}
